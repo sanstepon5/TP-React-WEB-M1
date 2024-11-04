070b617022a8afcba382e4cd6c4925aaf8d44107
Stepan Tyurin <sanstepon5@gmail.com>
2024-11-04 12:09:20 +0100

    add basic routing and about page

---
+++ tpreact/src/About.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+const About: React.FunctionComponent
+    = () => {
+    return <h1>Stepan</h1>;
+};
+
+export default About;
--- tpreact/src/index.tsx
+++ tpreact/src/index.tsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import "./index.css";
+import About from "./About";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "/about",
+        element: <About />,
+    },
+]);
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
 
