0ee47e296de110753b94dfa067068f914d1e6e5c
Yvan <yvandouis@hotmail.fr>
2024-11-04 12:43:43 +0100

    addbeer but you can enter the name youhou uuuuuuuuuuuuuuuuuu

--- tpreact/src/BeerList.tsx
+++ tpreact/src/BeerList.tsx
@@ -8,6 +8,7 @@ interface Props {
 
 interface State {
     beers: BeerData[];
+    newBeerName: string;
 }
 
 class BeerList extends React.Component<Props, State> {
@@ -15,14 +16,19 @@ class BeerList extends React.Component<Props, State> {
         super(props);
         this.state = {
             beers : [{name:"beer1"}],
+            newBeerName: "",
         };
     }
-    addBeer = (name: string): void => {
+    addBeer = (): void => {
         this.setState((state) => ({
-            beers: state.beers.concat({name}), // Add the new beer to the state
+            beers: state.beers.concat({name: state.newBeerName}), // Add the new beer to the state
+            newBeerName : ""
         }));
     };
+    handleInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        this.setState({ newBeerName: event.target.value });
 
+    };
 
     render() {
         return (
@@ -30,12 +36,19 @@ class BeerList extends React.Component<Props, State> {
                 <h1>Beer List</h1>
                 <ul>
                     {this.state.beers.map((beer, index) => (
-                        <Beer key={index} beer={beer} />
+                        <Beer key={index} beer={beer}/>
                     ))}
                 </ul>
-                <button onClick={() => this.addBeer(`Beer `)}>Add Beer</button>
+                <input
+                    type="text"
+                    value={this.state.newBeerName}
+                    onChange={this.handleInputChange}
+                    placeholder="Enter beer name"
+                />
+                <button onClick={() => this.addBeer()}>Add Beer</button>
             </div>
         );
     }
 }
+
 export default BeerList;
