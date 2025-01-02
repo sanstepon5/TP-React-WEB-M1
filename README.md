17ff92fcdea7c4fbbd0aabc161bd3256d42bd520
Stepan Tyurin <sanstepon5@gmail.com>
2025-01-02 19:48:36 +0100

    update readme and about page

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # TP React (WEB M1 IL)
 
-To run the project navigate to [tpreact](tpreact) and run
-`npm run start`.
+## Authors
+Stepan Tyurin, Yvan Douis - M1 Informatique IL
+
+2024-2025
+
+## Installation guide
+```
+git clone git@github.com:sanstepon5/TP-React-WEB-M1.git
+cd TP-React-WEB-M1/tpreact
+npm i
+npm run start
+```
+Then navigate to http://localhost:3000/ in your browser.
+
+## Application details
+This React app was made for a Web Engineering class.
+
+The application allows to visualize a list of beers and add or remove them. It also
+contains an "about" page available at http://localhost:4200/about thanks to routing
--- tpreact/src/About.tsx
+++ tpreact/src/About.tsx
@@ -2,7 +2,13 @@ import * as React from 'react';
 
 const About: React.FunctionComponent
     = () => {
-    return <h1>Stepan</h1>;
-};
+    return <><h1>TP React (WEB M1 IL)</h1><h2> Authors </h2><p>Stepan Tyurin, Yvan Douis - M1 Informatique IL</p>
+        <p>2024-2025</p>
+        <h2>Application details</h2><p>This
+            React app was made for a Web Engineering class.</p><p>The application allows to visualize a list of beers
+            and add or remove them. It also
+            contains an "about" page available at http://localhost:4200/about thanks to routing</p></>
+        ;
+        };
 
-export default About;
+        export default About;
